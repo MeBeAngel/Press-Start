6a026026947989a818eb58b276e4ed16ed4bf82a
MeBeAngel <58734248+MeBeAngel@users.noreply.github.com>
2020-07-23 19:54:47 -0400

    added gif to landing page

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let delayInMilliseconds = 2000; //2 second
+//let delayInMilliseconds = 2000; //2 second
 
-setTimeout(function() {
-  document.getElementById('Pixel').src="./Images/Pixel-Angel-Hello.png";
-}, delayInMilliseconds);
+//setTimeout(function() {
+//  document.getElementById('Pixel').src="./Images/Pixel-Angel-Hello.png";
+//}, delayInMilliseconds);
