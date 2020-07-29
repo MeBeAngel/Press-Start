44fe8c30c091a40d21c09783bf23ab90a1adb30c
MeBeAngel <58734248+MeBeAngel@users.noreply.github.com>
2020-07-29 18:40:22 -0400

    added nav items to landing page and onclick JS for each and directed each to home or continue pages

--- index.js
+++ index.js
@@ -3,3 +3,21 @@
 //setTimeout(function() {
 //  document.getElementById('Pixel').src="./Images/Pixel-Angel-Hello.png";
 //}, delayInMilliseconds);
+
+
+
+function handleStartClick () {
+
+  let start = document.getElementById('Start');
+  let newProject = document.getElementById('New-Project');
+  let load = document.getElementById('Continue');
+
+
+  if (start.style.display= 'block'){
+
+    start.style.display= 'none';
+    newProject.style.display= 'block';
+    load.style.display= 'block';
+  }
+  else {null}
+}
